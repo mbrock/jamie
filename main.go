0a86ad632a3f93b32167ca0f1b9ad1e73b915224
Mikael Brockman <mikael@brockman.se>
2024-07-22 18:08:35 +0300

    Removed copyright notice and implemented custom callback struct in main.go.

--- main.go
+++ main.go
@@ -152,11 +152,6 @@ func joinAllVoiceChannels(s *discordgo.Session, guildID string) error {
 	return nil
 }
 
-// Copyright 2023-2024 Deepgram SDK contributors. All Rights Reserved.
-// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
-// SPDX-License-Identifier: MIT
-
-// Implement your own callback
 type MyCallback struct {
 	sb *strings.Builder
 }
