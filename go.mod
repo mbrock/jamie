1f5fd0def9937e91ae7394ce5f5cda61cd447cb6
Mikael Brockman <mikael@brockman.se>
2024-07-22 17:58:08 +0300

    Added support for Deepgram speech-to-text API and microphone integration.

--- go.mod
+++ go.mod
@@ -5,14 +5,22 @@ go 1.20
 require (
 	github.com/bwmarrin/discordgo v0.28.1
 	github.com/charmbracelet/log v0.4.0
+	github.com/deepgram/deepgram-go-sdk v1.4.0
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/dvonthenen/websocket v1.5.1-dyv.2 // indirect
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5 // indirect
+	github.com/gorilla/schema v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
@@ -21,4 +29,5 @@ require (
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/sys v0.13.0 // indirect
+	k8s.io/klog/v2 v2.110.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
-	"io"
 	"os"
 	"os/signal"
 	"path/filepath"
@@ -12,6 +13,11 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/charmbracelet/log"
+
+	api "github.com/deepgram/deepgram-go-sdk/pkg/api/listen/v1/websocket/interfaces"
+	"github.com/deepgram/deepgram-go-sdk/pkg/audio/microphone"
+	interfaces "github.com/deepgram/deepgram-go-sdk/pkg/client/interfaces"
+	client "github.com/deepgram/deepgram-go-sdk/pkg/client/listen"
 )
 
 var (
@@ -152,7 +158,7 @@ func startRecording(v *discordgo.VoiceConnection, guildID, channelID string) {
 			logger.Info("Voice channel closed")
 			return
 		}
-		_, err = file.Write(opus)
+		_, err = file.Write(opus.Opus)
 		if err != nil {
 			logger.Error("Failed to write to file", "error", err)
 			return
@@ -222,4 +228,199 @@ func joinAllVoiceChannels(s *discordgo.Session, guildID string) error {
 	}
 
 	return nil
+}
+
+// Copyright 2023-2024 Deepgram SDK contributors. All Rights Reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+// SPDX-License-Identifier: MIT
+
+// Implement your own callback
+type MyCallback struct {
+	sb *strings.Builder
+}
+
+func (c MyCallback) Message(mr *api.MessageResponse) error {
+	// handle the message
+	sentence := strings.TrimSpace(mr.Channel.Alternatives[0].Transcript)
+
+	if len(mr.Channel.Alternatives) == 0 || len(sentence) == 0 {
+		return nil
+	}
+
+	if mr.IsFinal {
+		c.sb.WriteString(sentence)
+		c.sb.WriteString(" ")
+
+		if mr.SpeechFinal {
+			fmt.Printf("[------- Is Final]: %s\n", c.sb.String())
+			c.sb.Reset()
+		}
+	} else {
+		fmt.Printf("[Interm Result]: %s\n", sentence)
+	}
+
+	return nil
+}
+
+func (c MyCallback) Open(ocr *api.OpenResponse) error {
+	// handle the open
+	fmt.Printf("\n[Open] Received\n")
+	return nil
+}
+
+func (c MyCallback) Metadata(md *api.MetadataResponse) error {
+	// handle the metadata
+	fmt.Printf("\n[Metadata] Received\n")
+	fmt.Printf("Metadata.RequestID: %s\n", strings.TrimSpace(md.RequestID))
+	fmt.Printf("Metadata.Channels: %d\n", md.Channels)
+	fmt.Printf("Metadata.Created: %s\n\n", strings.TrimSpace(md.Created))
+	return nil
+}
+
+func (c MyCallback) SpeechStarted(ssr *api.SpeechStartedResponse) error {
+	fmt.Printf("\n[SpeechStarted] Received\n")
+	return nil
+}
+
+func (c MyCallback) UtteranceEnd(ur *api.UtteranceEndResponse) error {
+	utterance := strings.TrimSpace(c.sb.String())
+	if len(utterance) > 0 {
+		fmt.Printf("[------- UtteranceEnd]: %s\n", utterance)
+		c.sb.Reset()
+	} else {
+		fmt.Printf("\n[UtteranceEnd] Received\n")
+	}
+
+	return nil
+}
+
+func (c MyCallback) Close(ocr *api.CloseResponse) error {
+	// handle the close
+	fmt.Printf("\n[Close] Received\n")
+	return nil
+}
+
+func (c MyCallback) Error(er *api.ErrorResponse) error {
+	// handle the error
+	fmt.Printf("\n[Error] Received\n")
+	fmt.Printf("Error.Type: %s\n", er.Type)
+	fmt.Printf("Error.ErrCode: %s\n", er.ErrCode)
+	fmt.Printf("Error.Description: %s\n\n", er.Description)
+	return nil
+}
+
+func (c MyCallback) UnhandledEvent(byData []byte) error {
+	// handle the unhandled event
+	fmt.Printf("\n[UnhandledEvent] Received\n")
+	fmt.Printf("UnhandledEvent: %s\n\n", string(byData))
+	return nil
+}
+
+func foo() {
+	// init library
+	microphone.Initialize()
+
+	// print instructions
+	fmt.Print("\n\nPress ENTER to exit!\n\n")
+
+	/*
+		DG Streaming API
+	*/
+	// init library
+	client.Init(client.InitLib{
+		LogLevel: client.LogLevelDefault, // LogLevelDefault, LogLevelFull, LogLevelDebug, LogLevelTrace
+	})
+
+	// Go context
+	ctx := context.Background()
+
+	// client options
+	cOptions := &interfaces.ClientOptions{
+		EnableKeepAlive: true,
+	}
+
+	// set the Transcription options
+	tOptions := &interfaces.LiveTranscriptionOptions{
+		Model:       "nova-2",
+		Language:    "en-US",
+		Punctuate:   true,
+		Encoding:    "linear16",
+		Channels:    1,
+		SampleRate:  16000,
+		SmartFormat: true,
+		VadEvents:   true,
+		// To get UtteranceEnd, the following must be set:
+		InterimResults: true,
+		UtteranceEndMs: "1000",
+		// End of UtteranceEnd settings
+	}
+
+	// example on how to send a custom parameter
+	// params := make(map[string][]string, 0)
+	// params["dictation"] = []string{"true"}
+	// ctx = interfaces.WithCustomParameters(ctx, params)
+
+	// implement your own callback
+	callback := MyCallback{
+		sb: &strings.Builder{},
+	}
+
+	// create a Deepgram client
+	dgClient, err := client.NewWebSocket(ctx, "", cOptions, tOptions, callback)
+	if err != nil {
+		fmt.Println("ERROR creating LiveTranscription connection:", err)
+		return
+	}
+
+	// connect the websocket to Deepgram
+	bConnected := dgClient.Connect()
+	if !bConnected {
+		fmt.Println("Client.Connect failed")
+		os.Exit(1)
+	}
+
+	/*
+		Microphone package
+	*/
+	// mic stuf
+	mic, err := microphone.New(microphone.AudioConfig{
+		InputChannels: 1,
+		SamplingRate:  16000,
+	})
+	if err != nil {
+		fmt.Printf("Initialize failed. Err: %v\n", err)
+		os.Exit(1)
+	}
+
+	// start the mic
+	err = mic.Start()
+	if err != nil {
+		fmt.Printf("mic.Start failed. Err: %v\n", err)
+		os.Exit(1)
+	}
+
+	go func() {
+		// feed the microphone stream to the Deepgram client (this is a blocking call)
+		mic.Stream(dgClient)
+	}()
+
+	// wait for user input to exit
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+
+	// close mic stream
+	err = mic.Stop()
+	if err != nil {
+		fmt.Printf("mic.Stop failed. Err: %v\n", err)
+		os.Exit(1)
+	}
+
+	// teardown library
+	microphone.Teardown()
+
+	// close DG client
+	dgClient.Stop()
+
+	fmt.Printf("\n\nProgram exiting...\n")
+	// time.Sleep(120 * time.Second)
 }
