697ef7f6f6ef7cc08ede2b120e3db98ba4682ab7
Mikael Brockman <mikael@brockman.se>
2024-08-06 00:11:29 +0300

    feat: Improve JSON rendering with better formatting

--- discord/json_renderer.go
+++ discord/json_renderer.go
@@ -1,8 +1,9 @@
 package discord
 
 import (
-	"encoding/json"
 	"fmt"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/charmbracelet/lipgloss"
@@ -11,7 +12,6 @@ import (
 var (
 	keyStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#87CEEB"))
 	valueStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#98FB98"))
-	braceStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFA07A"))
 )
 
 func RenderJSON(data interface{}) string {
@@ -23,36 +23,29 @@ func RenderJSON(data interface{}) string {
 func renderJSONValue(v interface{}, indent int, sb *strings.Builder) {
 	switch val := v.(type) {
 	case map[string]interface{}:
-		sb.WriteString("{\n")
+		sb.WriteString("\n")
 		keys := make([]string, 0, len(val))
 		for k := range val {
 			keys = append(keys, k)
 		}
 		sort.Strings(keys)
-		for i, k := range keys {
+		for _, k := range keys {
 			writeIndent(indent+1, sb)
 			sb.WriteString(keyStyle.Render(k))
 			sb.WriteString(": ")
 			renderJSONValue(val[k], indent+1, sb)
-			if i < len(keys)-1 {
-				sb.WriteString(",")
-			}
 			sb.WriteString("\n")
 		}
 		writeIndent(indent, sb)
-		sb.WriteString("}")
 	case []interface{}:
-		sb.WriteString("[\n")
+		sb.WriteString("\n")
 		for i, item := range val {
 			writeIndent(indent+1, sb)
+			sb.WriteString(fmt.Sprintf("%d: ", i))
 			renderJSONValue(item, indent+1, sb)
-			if i < len(val)-1 {
-				sb.WriteString(",")
-			}
 			sb.WriteString("\n")
 		}
 		writeIndent(indent, sb)
-		sb.WriteString("]")
 	case string:
 		sb.WriteString(valueStyle.Render(fmt.Sprintf("%q", val)))
 	case float64:
