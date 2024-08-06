1481b62fe5d6daa4292153e5c92a82e526dc452b
Mikael Brockman <mikael@brockman.se>
2024-08-06 13:37:51 +0300

    feat: Add module declaration and improve code formatting

--- setup/setup.pl
+++ setup/setup.pl
@@ -1,96 +1,106 @@
 % Mock predicates for simulation
 
+:- module(setup,
+          [ run_setup/0
+	  ]).
+
 log(Level, Message) :-
-    format('LOG (~w): ~w~n', [Level, Message]).
+	format('LOG (~w): ~w~n', [Level, Message]).
 
 run_command(Command, Result) :-
-    format('Would run command: ~w~n', [Command]),
-    format('Succeed or fail? (s/f): '),
-    read_line_to_string(user_input, Response),
-    (Response = "s" -> Result = success ; Result = fail).
+	format('Would run command: ~w~n', [Command]),
+	format('Succeed or fail? (s/f): '),
+	read_line_to_string(user_input, Response),
+	(Response = 's' ->
+		Result = success;
+		Result = fail).
 
 open_database(Result) :-
-    format('Simulating database opening.~n'),
-    format('Succeed or fail? (s/f): '),
-    read_line_to_string(user_input, Response),
-    (Response = "s" -> Result = success ; Result = fail).
+	format('Simulating database opening.~n'),
+	format('Succeed or fail? (s/f): '),
+	read_line_to_string(user_input, Response),
+	(Response = 's' ->
+		Result = success;
+		Result = fail).
 
 set_config(Key, Value) :-
-    format('Setting config: ~w = ~w~n', [Key, Value]).
+	format('Setting config: ~w = ~w~n', [Key, Value]).
 
 ask(Prompt, Answer) :-
-    format('~w', [Prompt]),
-    read_line_to_string(user_input, Answer).
+	format('~w', [Prompt]),
+	read_line_to_string(user_input, Answer).
 
 confirm(Prompt, Answer) :-
-    format('~w (y/n): ', [Prompt]),
-    read_line_to_string(user_input, Response),
-    (Response = "y" -> Answer = yes ; Answer = no).
+	format('~w (y/n): ', [Prompt]),
+	read_line_to_string(user_input, Response),
+	(Response = "y" ->
+		Answer = yes;
+		Answer = no).
 
 jamie_username('jamie').
 jamie_password('jamie').
 db_name('jamie').
 
 run_setup :-
-	log(info, 'Starting Jamie setup...'), ensure_system_user, !, ensure_database_user, !, ensure_database, !, 
+	log(info, 'Starting Jamie setup...'), ensure_system_user, !, ensure_database_user, !, ensure_database, !,
 	open_database(success), prompt_for_api_keys.
 
 ensure_system_user :-
-	jamie_username(Username), 
-	system_user_exists(Username), 
-	log(info, 'System user already exists').
+	jamie_username(Username),
+	system_user_exists(Username),
+	log(info, 'System user already exists'), !.
 
 ensure_system_user :-
-	jamie_username(Username), 
-	log(info, 'Creating system user'), 
-	create_system_user(Username), 
+	jamie_username(Username),
+	log(info, 'Creating system user'),
+	create_system_user(Username),
 	system_user_exists(Username).
 
 create_system_user(Username) :-
-	run_command(['useradd', '-r', '-s', '/bin/false', Username]), 
+	run_command(['useradd', '-r', '-s', '/bin/false', Username]),
 	log(info, 'System user created successfully').
 
 ensure_database_user :-
-	jamie_username(Username), 
-	database_user_exists(Username).
+	jamie_username(Username),
+	database_user_exists(Username), !.
 
 ensure_database_user :-
-	jamie_username(Username), 
-	jamie_password(Password), 
-	log(info, 'Creating database user'), 
-	create_database_user(Username, Password), 
-	database_user_exists(Username).
+	jamie_username(Username),
+	jamie_password(Password),
+	log(info, 'Creating database user'),
+	create_database_user(Username, Password),
+	database_user_exists(Username), !.
 
 create_database_user(Username, Password) :-
-	run_command(['createuser', '-s', Username]), 
+	run_command(['createuser', '-s', Username]),
 	set_database_user_password(Username, Password).
 
 set_database_user_password(Username, Password) :-
 	format(
-		atom(AlterCommand), 'ALTER USER ~w WITH PASSWORD \'~w\';', [Username, Password]), 
-	run_command(['psql', '-c', AlterCommand]), 
+		atom(AlterCommand), 'ALTER USER ~w WITH PASSWORD \'~w\';', [Username, Password]),
+	run_command(['psql', '-c', AlterCommand]),
 	log(info, 'Database user created successfully').
 
 ensure_database :-
-	db_name(DbName), 
-	database_exists(DbName), 
+	db_name(DbName),
+	database_exists(DbName), !,
 	log(info, 'Database already exists').
 
 ensure_database :-
-	db_name(DbName), 
-	jamie_username(Owner), 
-	log(info, 'Creating database'), 
-	create_database(DbName, Owner), 
+	db_name(DbName),
+	jamie_username(Owner),
+	log(info, 'Creating database'),
+	create_database(DbName, Owner),
 	database_exists(DbName).
 
 create_database(DbName, Owner) :-
-	run_command(['createdb', '-O', Owner, DbName]), 
-	log(info, 'Database created successfully'), 
+	run_command(['createdb', '-O', Owner, DbName]),
+	log(info, 'Database created successfully'),
 	initialize_database_schema(DbName, Owner).
 
 initialize_database_schema(DbName, Owner) :-
-	log(info, 'Initializing database schema...'), 
-	run_command(['psql', '-d', DbName, '-f', 'db/db_init.sql']), 
+	log(info, 'Initializing database schema...'),
+	run_command(['psql', '-d', DbName, '-f', 'db/db_init.sql']),
 	log(info, 'Database schema initialized successfully').
 
 % Helper predicates to check existence
@@ -100,7 +110,7 @@ system_user_exists(Username) :-
 
 database_user_exists(Username) :-
 	format(
-		atom(Query), 'SELECT 1 FROM pg_roles WHERE rolname=\'~w\'', [Username]), 
+		atom(Query), 'SELECT 1 FROM pg_roles WHERE rolname=\'~w\'', [Username]),
 	run_command(['psql', '-tAc', Query], '1\n').
 
 database_exists(DbName) :-
@@ -109,20 +119,20 @@ database_exists(DbName) :-
 % Run command with potential sudo
 
 run_command(Command) :-
-	run_command(Command, success).
+	run_command(Command, success), !.
 
 run_command(Command) :-
-	confirm('Use sudo?', yes), 
-	append(['sudo'], Command, SudoCommand), 
+	confirm('Use sudo?', yes),
+	append(['sudo'], Command, SudoCommand),
 	run_command(SudoCommand, success).
 
 prompt_for_api_keys :-
-	prompt_for_api_key('Discord', 'DISCORD_TOKEN'), 
-	prompt_for_api_key('Google Cloud (Gemini)', 'GEMINI_API_KEY'), 
+	prompt_for_api_key('Discord', 'DISCORD_TOKEN'),
+	prompt_for_api_key('Google Cloud (Gemini)', 'GEMINI_API_KEY'),
 	prompt_for_api_key('Speechmatics', 'SPEECHMATICS_API_KEY').
 
 prompt_for_api_key(Service, Key) :-
 	format(
-		atom(Prompt), 'Enter API key for ~w: ', [Service]), 
-	ask(Prompt, Value), 
+		atom(Prompt), 'Enter API key for ~w: ', [Service]),
+	ask(Prompt, Value),
 	set_config(Key, Value).
